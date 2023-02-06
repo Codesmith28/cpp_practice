90284e8d300c009b46e87e1fea28ae57d2ded9ce
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-02-06 22:54:02 +0530

    comparator

--- custom_comparator/manual_comparators.cpp
+++ custom_comparator/manual_comparators.cpp
@@ -1,17 +1,48 @@
 #include <bits/stdc++.h>
 #define vi vector<int>
-#define pii pair<int,int>
+#define pii pair<int, int>
 #define vii vector<pii>
-#define rep(i,a,b) for(int i=a; i<b; i++)
+#define rep(i, a, b) for (int i = a; i < b; i++)
 #define ff first
 #define ss second
 
 using namespace std;
 
+// sort wrt to ss
+bool comp_ss(pii p1, pii p2)
+{
+    // decreasing order:
+    // return p1.ss > p2.ss;
+
+    // increasing order:
+    return p1.ss < p2.ss ;
+}
+
+
 int main()
 {
     int n;
-    cin>>n;
-    
+    cin >> n;
+
+    vii v;
+    rep(i, 0, n)
+    {
+        int x, y;
+        cin >> x >> y;
+        v.push_back({x, y});
+    }
+
+    // normal sorting:
+    // by default considers ff
+    // sort(v.begin(), v.end());
+
+    // manaul:
+    sort(v.begin(), v.end(), comp_ss);
+
+    rep(i, 0, n)
+    {
+        cout << v[i].ff << "  " << v[i].ss << endl;
+    }
+    cout << "\n";
     return 0;
 }
